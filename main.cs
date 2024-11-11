895c8c38703f61b72ad9586b5aadc99ceecd3ec6
Samara Angélica <contato.angelica07@gmail.com>
2024-11-11 17:50:21 -0300

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    Console.Write("Digite a sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+    
+if (idade >= 70) {
+  Console.WriteLine("Você  tem 70 ou mais anos e seu voto é opcional.");
+}
+    else if (idade >= 18) {
+      Console.WriteLine ("Você é uma pessoa maior de idade e seu voto é obrigatório.");
+    }
+    else if (idade >= 16) {
+    Console.WriteLine ("Seu voto ainda é opçãonal.");
+  
+  }
+    else {
+      Console.WriteLine ("Você ainda não pode votar, pois é muito jovem.");
+    }
+    
+    }
+  }
