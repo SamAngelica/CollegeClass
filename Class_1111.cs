ed76179f5cf0872c1740403759d4736d2002824b
Samara Angélica <contato.angelica07@gmail.com>
2024-11-11 17:59:50 -0300

    Update Class_1111.cs

--- Class_1111.cs
+++ Class_1111.cs
@@ -5,20 +5,25 @@ class Program {
 
     Console.Write("Digite a sua idade: ");
 int idade = int.Parse(Console.ReadLine());
+
+    if(idade > 0){
     
-if (idade >= 70) {
-  Console.WriteLine("Você  tem 70 ou mais anos e seu voto é opcional.");
-}
-    else if (idade >= 18) {
-      Console.WriteLine ("Você é uma pessoa maior de idade e seu voto é obrigatório.");
-    }
-    else if (idade >= 16) {
-    Console.WriteLine ("Seu voto ainda é opçãonal.");
-  
+  if (idade >= 70) {
+    Console.WriteLine("Você  tem 70 ou mais anos e seu voto é opcional.");
   }
-    else {
-      Console.WriteLine ("Você ainda não pode votar, pois é muito jovem.");
-    }
+      else if (idade >= 18) {
+        Console.WriteLine ("Você é uma pessoa maior de idade e seu voto é obrigatório.");
+      }
+      else if (idade >= 16) {
+      Console.WriteLine ("Seu voto ainda é opçãonal.");
     
     }
-  }
+      else {
+        Console.WriteLine ("Você ainda não pode votar, pois é muito jovem.");
+    }
+      
+      }
+    else 
+      {
+      Console.WriteLine("Digite um número válido, não tem como ter uma idade negativa.");
+      }}}
