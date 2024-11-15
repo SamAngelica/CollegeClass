bb844f9c414c30aed24ae67bcff85b908273b820
Samara Angélica <contato.angelica07@gmail.com>
2024-11-15 12:13:45 -0300

    Add files via upload

---
+++ Class4_C#.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+     int cont = 0;
+
+    while (cont <= 10){
+    Console.WriteLine("Contador: " + cont);
+    cont += 1;
+      
+      }
+
+    }
+
+  }
+    
+  
